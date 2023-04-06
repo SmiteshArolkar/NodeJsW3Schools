cc0336c7b3ce12c7e1c50e058796c65fd4c0f117
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-04-06 23:47:10 +0530

    Query Url

--- node.js
+++ node.js
@@ -4,5 +4,6 @@ var dt = require('./dateTimeModule');
 http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("Date And Time are " + dt.dateTime())
+  res.write(req.url)
   res.end()
 }).listen(8080)
