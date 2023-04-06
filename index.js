aa33dbf3a0518568a453fd5ba465ef1ff93967b1
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-04-07 00:14:02 +0530

    Read And Write to fs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http')
-var fs = require('fs')
+var fs = require('fs');
+const { error } = require('console');
 
 http.createServer(function (req,res){
     fs.readFile('index.html',function(err,data) {
@@ -7,8 +8,18 @@ http.createServer(function (req,res){
         res.write(data)
         return res.end();
     })
-    fs.appendFile('newFile.txt',"Hello conteent!",(err) => {
-        if(err) throw err
-        console.log('Saved')
+    // fs.appendFile('newFile.txt',"Hello conteent!",(err) => {
+    //     if(err) throw err
+    //     console.log('Saved')
+    // })
+    // fs.readFile('newFile.txt',(err,data) => {
+    //     res.writeHead(200,{'Context-Type':'text/html'});
+    //     res.write(data)
+    //     if(err) throw err
+    //     console.log("Read Success")
+    // })
+    fs.writeFile('myFile.txt',"This is Test Write",(error) => {
+        if(error) throw error
+        console.log("write Success")
     })
 }).listen(8080);
